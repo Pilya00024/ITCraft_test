4364f97530267944699383bdcd2c83dab99618c8
Vyacheslav Pilipenko <Pilya00024@gmail.com>
2019-09-29 11:15:34 +0300

    add default Route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router';
+import { Redirect } from 'react-router-dom';
 import routeNames from './utils/routeNames';
 import { loadable } from './utils/loadable';
+import { connect } from 'react-redux';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
 import './App.scss';
 
-function App() {
+function App({isAuthorization}) {
+  console.log(this)
   return (
     <Suspense fallback={<LoadingSpinner />}>
       <div className="App">
         <Switch>
-          <PrivateRoute exact path={'/'} />
+          <Redirect exact from="/" to={ isAuthorization ? routeNames.market : routeNames.login } />
           <Route exact path={routeNames.login} component={loadable.login} />
           <PrivateRoute exact path={routeNames.market} component={loadable.market} />
           <PrivateRoute exact path={routeNames.cart} component={loadable.cart} />
@@ -21,4 +24,8 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  isAuthorization: state.currentUser.isAuthorization,
+});
+
+export default connect(mapStateToProps, null)(App);
